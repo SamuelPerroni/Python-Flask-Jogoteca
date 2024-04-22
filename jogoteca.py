13b53d8aa53f355fd6f644653bfb4d689bce01f4
Samuel Perroni <samuel_perroni@outlook.com>
2024-04-22 16:38:02 -0300

    adicionando segurança com o BCrypt

--- jogoteca.py
+++ jogoteca.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import BCrypt
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
+bcrypt = BCrypt(app)
 
-from views import *
+from views_game import *
+from views_user import *
 
 if __name__ == '__main__':
     app.run(debug=True)
